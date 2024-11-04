bfe6df145206a650dde1474315f50d185f4d3650
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-04 21:24:07 +0900

    j

---
+++ section05/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header.jsx";
+import main from "./components/main.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ section05/src/components/main.jsx
@@ -0,0 +1,9 @@
+const main = () => {
+    return (
+        <div>
+            <h1>main</h1>
+        </div>
+    );
+};
+
+export default main;
