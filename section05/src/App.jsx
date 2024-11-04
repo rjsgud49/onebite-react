246fd30b117a2f6ed452c6278051ddadc6f06bc2
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-04 21:40:07 +0900

    f

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Header from "./components/header.jsx";
-import main from "./components/main.jsx";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
     return (
         <>
             <Header />
-            <main />
+            <Main />
             <Footer />
         </>
     );
