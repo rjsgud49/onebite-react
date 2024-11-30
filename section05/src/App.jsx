d5e83e12cbeb4d59d76d7448872370efaa723d84
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-30 09:11:04 +0900

    dd

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import Button from "./components/Button.jsx";
 
 function App() {
     return (
         <>
-            <Main />
+            <Button text={"메일"} />
+            {/* <Button text={"카페"} />
+            <Button text={"블로그"} /> */}
         </>
     );
 }
---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,6 @@
+const Butten = (props) => {
+    console.log(props);
+    return <Butten>클릭</Butten>;
+};
+
+export default Butten;
